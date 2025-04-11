03f6b74f6b7a02df93114bbe7715dff61d5dc64e
Thierry Thoua <titi@lelibre.net>
2025-04-11 16:39:32 +0000

    new version of docker filed

--- shiori/run.sh
+++ shiori/run.sh
@@ -3,5 +3,5 @@
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 
 # Start Shiori with s6
-exec /usr/local/bin/shiori server --port 8080 --dir /data
+exec /usr/local/bin/shiori server --port 8080 --dir /data --debug
 
