ac0ef6dbd0614b50e8390c1a974ad82db581709e
titidnh <titi@lelibre.net>
2025-12-28 14:12:30 +0100

    Change unbound-anchor to exec for better handling

--- unbound/run.sh
+++ unbound/run.sh
@@ -4,5 +4,5 @@ set -e
 curl -o /etc/unbound/root.hints https://www.internic.net/domain/named.root
 chown root:root /etc/unbound/root.hints
 chmod 644 /etc/unbound/root.hints
-unbound-anchor -a /etc/unbound/root.key -vvv
+exec unbound-anchor -a /etc/unbound/root.key -vvv
 exec /usr/sbin/unbound -v -d
