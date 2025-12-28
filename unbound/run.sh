fa50b383da04dd0bdac6a02a382e9199f6a14831
titidnh <titi@lelibre.net>
2025-12-28 14:17:37 +0100

    Simplify run.sh by removing unneeded commands
    Removed DNSSEC root key generation from script.

--- unbound/run.sh
+++ unbound/run.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 set -e
-
 # Download/update root hints
 curl -s -o /etc/unbound/root.hints https://www.internic.net/domain/named.root
 chown root:root /etc/unbound/root.hints
 chmod 644 /etc/unbound/root.hints
-
-# Generate/update DNSSEC root key
-unbound-anchor -a /etc/unbound/root.key -vvv
-
-# Start Unbound in foreground (systemd will manage it)
 exec /usr/sbin/unbound -v
