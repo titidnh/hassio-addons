baa6954bfb0b1725220988a0ec732e54fdc5f68c
titidnh <titi@lelibre.net>
2025-12-28 14:05:53 +0100

    Update run.sh

--- unbound/run.sh
+++ unbound/run.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 set -e
 
-curl -o /etc/unbound/root.hints https://www.internic.net/domain/named.root
+#curl -o /etc/unbound/root.hints https://www.internic.net/domain/named.root
+sudo curl -o /etc/unbound/root.hints https://www.internic.net/domain/named.root
+sudo chown root:root /etc/unbound/root.hints
+sudo chmod 644 /etc/unbound/root.hints
+sudo unbound-anchor -a /etc/unbound/root.key
+ls -l /etc/unbound/root.key
 exec /usr/sbin/unbound -v -d
