f009e6c1e12add770076d68c8d3fde26ca9cfd0d
titidnh <titi@lelibre.net>
2025-12-28 14:08:38 +0100

    Remove sudo from unbound/run.sh commands

--- unbound/run.sh
+++ unbound/run.sh
@@ -2,9 +2,9 @@
 set -e
 
 #curl -o /etc/unbound/root.hints https://www.internic.net/domain/named.root
-sudo curl -o /etc/unbound/root.hints https://www.internic.net/domain/named.root
-sudo chown root:root /etc/unbound/root.hints
-sudo chmod 644 /etc/unbound/root.hints
-sudo unbound-anchor -a /etc/unbound/root.key
+curl -o /etc/unbound/root.hints https://www.internic.net/domain/named.root
+chown root:root /etc/unbound/root.hints
+chmod 644 /etc/unbound/root.hints
+unbound-anchor -a /etc/unbound/root.key
 ls -l /etc/unbound/root.key
 exec /usr/sbin/unbound -v -d
