91a2f9969e3253d3236c11644b1c7bbbf5dcba36
titidnh <titi@lelibre.net>
2025-12-28 14:15:52 +0100

    Refactor run.sh to improve clarity and functionality
    Updated script to download root hints silently and added comments for clarity.

--- unbound/run.sh
+++ unbound/run.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 set -e
 
-curl -o /etc/unbound/root.hints https://www.internic.net/domain/named.root
+# Download/update root hints
+curl -s -o /etc/unbound/root.hints https://www.internic.net/domain/named.root
 chown root:root /etc/unbound/root.hints
 chmod 644 /etc/unbound/root.hints
-exec unbound-anchor -a /etc/unbound/root.key -vvv
-exec /usr/sbin/unbound -v -d
+
+# Generate/update DNSSEC root key
+unbound-anchor -a /etc/unbound/root.key -vvv
+
+# Start Unbound in foreground (systemd will manage it)
+exec /usr/sbin/unbound -v
