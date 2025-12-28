a95114478611db33ecc98baa420309536afde317
titidnh <titi@lelibre.net>
2025-12-28 14:09:17 +0100

    Fix curl command to ensure root.hints is downloaded

--- unbound/run.sh
+++ unbound/run.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 set -e
 
-#curl -o /etc/unbound/root.hints https://www.internic.net/domain/named.root
 curl -o /etc/unbound/root.hints https://www.internic.net/domain/named.root
 chown root:root /etc/unbound/root.hints
 chmod 644 /etc/unbound/root.hints
 unbound-anchor -a /etc/unbound/root.key
 ls -l /etc/unbound/root.key
+
 exec /usr/sbin/unbound -v -d
